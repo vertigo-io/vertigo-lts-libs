680471af40b6a7a6616ad89d79ad0a94c378c729
Piedeloup Nicolas <nicolas.piedeloup@kleegroup.com>
2021-02-04 17:47:43 +0100

    [Struts2] Fix UI tests

--- vertigo-struts2/src/test/java/io/vertigo/struts2/TestUi.java
+++ vertigo-struts2/src/test/java/io/vertigo/struts2/TestUi.java
@@ -317,9 +317,7 @@ public class TestUi {
 
 		findElement(By.xpath("//form[@id='autocompleteContextMdl']/table/tbody/tr/td/input[2]")).clear();
 		findElement(By.xpath("//form[@id='autocompleteContextMdl']/table/tbody/tr/td/input[2]")).sendKeys("blai");
-		Thread.sleep(6000);
 		assertEquals("BLAISE", waitElement(By.cssSelector("ul.ui-autocomplete span.col"), 10000).getText());
-
 		findElement(By.cssSelector("ul.ui-autocomplete span.col")).click();
 		assertEquals("BLAISE", findElement(By.xpath("//form[@id='autocompleteContextMdl']/table/tbody/tr/td/input[2]")).getAttribute("value"));
 
@@ -415,6 +413,8 @@ public class TestUi {
 		do {
 			try {
 				if (isElementPresent(byElement)) {
+					//System.out.println(byElement.toString() + " found in " + (System.currentTimeMillis() - start));
+					Thread.sleep(250);
 					return driver.findElement(byElement);
 				}
 			} catch (final Exception e) {
