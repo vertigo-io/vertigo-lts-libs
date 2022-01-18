ffaf847089bfca6c3c5b6ab62f245d172d80e759
Matthieu Laroche <matthieu.laroche@telecom-bretagne.eu>
2022-01-18 15:28:04 +0100

    [all] update dependencies

--- vertigo-datafactory-plugin-elasticsearch_5_6/src/test/java/io/vertigo/datafactory/search_5_6/withstore/SearchManagerStoreTest.java
+++ vertigo-datafactory-plugin-elasticsearch_5_6/src/test/java/io/vertigo/datafactory/search_5_6/withstore/SearchManagerStoreTest.java
@@ -151,8 +151,8 @@ public class SearchManagerStoreTest {
 
 		//A chaque test on recrée la table famille
 		try (final SqlConnectionCloseable connectionCloseable = new SqlConnectionCloseable(dataBaseManager)) {
-			execCallableStatement(connectionCloseable.getConnection(), "create table item(ID BIGINT, MANUFACTURER varchar(50), MODEL varchar(255), DESCRIPTION varchar(512), FAM_ID BIGINT, YEAR INT, KILO INT, PRICE INT, CONSOMMATION NUMERIC(8,2), MOTOR_TYPE varchar(50), OPTIONAL_NUMBER BIGINT, OPTIONAL_STRING varchar(50), LOCALISATION varchar(50), LAST_MODIFIED timestamp, PRECISION INT );");
-			execCallableStatement(connectionCloseable.getConnection(), "create sequence SEQ_ITEM start with 10001 increment by 1");
+			execCallableStatement(connectionCloseable.getConnection(), "create table item(ID BIGINT, MANUFACTURER varchar(50), MODEL varchar(255), DESCRIPTION varchar(512), FAM_ID BIGINT, ITEM_YEAR INT, KILO INT, PRICE INT, CONSOMMATION NUMERIC(8,2), MOTOR_TYPE varchar(50), OPTIONAL_NUMBER BIGINT, OPTIONAL_STRING varchar(50), LOCALISATION varchar(50), LAST_MODIFIED timestamp, PRECISION INT );");
+			execCallableStatement(connectionCloseable.getConnection(), "create sequence SEQ_ITEM start with 10000 increment by 1");
 		}
 
 		//On supprime tout
@@ -363,7 +363,7 @@ public class SearchManagerStoreTest {
 		item.setPrice(12000);
 		item.setManufacturer("Acme");
 		item.setModel("Martin");
-		item.setYear(1978);
+		item.setItemYear(1978);
 		item.setKilo(1500);
 		final BigDecimal conso = new BigDecimal(7.6);
 		conso.setScale(2, RoundingMode.HALF_UP);
